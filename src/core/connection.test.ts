26cf0d6d7aa2c3d176855b7071f77915a6d48216
Ian <ian@zentrack.cl>
2024-02-11 23:50:50 +0000

    Testing as admin and user.

--- src/core/connection.test.ts
+++ src/core/connection.test.ts
@@ -1,8 +1,8 @@
 import {Connection} from "./connection";
 
-it('Connection::constructor should assign the options', () => {
+it('[Administrator] Connection::constructor should assign the options', async () => {
     const connection = new Connection({
-        authorization: process.env.TOKEN as string,
+        authorization: process.env.ADMIN_TOKEN as string,
         address: process.env.ADDRESS as string,
         port: Number.parseInt(process.env.PORT as string),
         secure: true,
@@ -10,10 +10,29 @@ it('Connection::constructor should assign the options', () => {
         secret: process.env.SECRET as string,
     });
 
-    expect(connection.options.authorization).toBe(process.env.TOKEN)
+    expect(connection.options.authorization).toBe(process.env.ADMIN_TOKEN)
     expect(connection.options.address).toBe(process.env.ADDRESS)
     expect(connection.options.port).toBe(Number.parseInt(process.env.PORT as string))
     expect(connection.options.secure).toBe(true)
     expect(connection.options.debug).toBe(true)
     expect(connection.options.secret).toBe(process.env.SECRET)
+
+    const instance = await connection.connect()
+    instance.disconnect();
+});
+
+it('[User] Connection::constructor should assign the options', async () => {
+    const connection = new Connection({
+        authorization: process.env.USER_TOKEN as string,
+        address: process.env.ADDRESS as string,
+        port: Number.parseInt(process.env.PORT as string),
+        secure: true,
+        debug: true,
+        secret: process.env.SECRET as string,
+    });
+
+    expect(connection.options.authorization).toBe(process.env.USER_TOKEN)
+
+    const instance = await connection.connect()
+    instance.disconnect();
 });
